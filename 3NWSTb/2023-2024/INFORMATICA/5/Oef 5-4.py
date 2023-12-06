5e43c7fe2267ebbe3df7e0efe074fbefe023fc7e
samvandenabeele <150257581+samvandenabeele@users.noreply.github.com>
2023-12-06 11:26:38 +0100

    Update Oef 5-4.py

--- 3NWSTb/2023-2024/INFORMATICA/5/Oef 5-4.py
+++ 3NWSTb/2023-2024/INFORMATICA/5/Oef 5-4.py
@@ -1,6 +1,8 @@
 x = 0
-for x in range(0, 10):
-    x += input(f"Wat is het {x}de getal")
+y = 0
+for i in range(0, 10):
+    x = int(input(f"Wat is het {i + 1}de getal\n"))
+    y = y + x
 
-gemiddelde = x/10
+gemiddelde = y/10
 print(f"\n\nHet gemiddelde van de getallen is {gemiddelde}")
